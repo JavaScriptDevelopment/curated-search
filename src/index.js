f9ab12c352a3d5b58ec9c8742d2109366dde1db7
A.Rupesh <a.rupesh66@gmail.com>
2018-09-01 12:19:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetails';
+
+const API_KEY = ' ';
 
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+    
+
+        this.videoSearch('');
+    }
+
