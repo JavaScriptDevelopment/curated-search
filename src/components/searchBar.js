67d00a045f4af23b67d4bfa62dbbc237671378ce
A.Rupesh <a.rupesh66@gmail.com>
2018-09-01 12:21:04 +0530

    Update searchBar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,1 +1,21 @@
 
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+    }
+    render() {
+        return (
+            <div className="search-bar">
+                <input
+                  value={this.state.term}
+                  onChange={event => this.onInputChange(event.target.value)}
+                />
+            </div>
+        );
+    };
+    
