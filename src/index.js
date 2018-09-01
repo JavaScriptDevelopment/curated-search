35eaa84c53b88a9bc808dc657ddabc1b6d8c3bee
A.Rupesh <a.rupesh66@gmail.com>
2018-09-01 12:32:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,9 +15,34 @@ class App extends Component {
         this.state = {
             videos: [],
             selectedVideo: null
-        };
-    
+        };    
 
         this.videoSearch('');
     }
 
+ videoSearch(term) {
+	    YTSearch({key: API_KEY, term: term}, (videos) => {
+		    this.setState({
+			    videos: videos,
+			    selectedVideo: videos[0]
+		    });
+	    });
+    }
+    
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+      
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+	                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                  videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector(".container"));
